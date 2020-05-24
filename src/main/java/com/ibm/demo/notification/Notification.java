1fa4ee0962461d05ae21185ebe300fa32ed6319c
Appar <apperu09@in.ibm.com>
2020-05-24 07:35:25 +0530

    Code refactoring and added notification.

---
+++ src/main/java/com/ibm/demo/notification/Notification.java
@@ -0,0 +1,44 @@
+package com.ibm.demo.notification;
+
+import java.time.ZonedDateTime;
+import java.util.Objects;
+
+public class Notification {
+    private String message;
+    private ZonedDateTime timestamp;
+
+    public Notification(String message, ZonedDateTime timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public ZonedDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(ZonedDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Notification that = (Notification) o;
+        return message.equals(that.message) &&
+                timestamp.equals(that.timestamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message, timestamp);
+    }
+}
--- src/main/java/com/ibm/demo/student/StudentCourse.java
+++ src/main/java/com/ibm/demo/student/StudentCourse.java
@@ -1,4 +1,4 @@
-package com.amigoscode.demo.student;
+package com.ibm.demo.student;
 
 import java.time.LocalDate;
 import java.util.UUID;
